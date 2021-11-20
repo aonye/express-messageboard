bc32c711aa441b8deae27e6302d7f56d0f107e7d
Andy Ye <ye.andyy@gmail.com>
2021-11-19 21:26:34 -0500

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var router = express.Router();
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
+/* GET home page. */
+router.get('/', (req, res, next) => {
+  res.render('index', { title: "Mini Messageboard", messages: messages })
+});
+
+router.get('/new', (req, res, next) => {
+  res.render('form', { title: "Mini Messageboard", messages: messages })
+});
+
+router.post('/new', (req, res, next) => {
+  res.send('hello world');
+});
+
+
+module.exports = router;
